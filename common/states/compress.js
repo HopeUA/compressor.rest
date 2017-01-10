56cbf962485218e719185a9003a9661b3de53f7f
Sergey Sadovoi <serg.sadovoi@gmail.com>
2017-01-10 15:06:30 +0200

    [new] Compress handler: Decrease audio level

--- common/states/compress.js
+++ common/states/compress.js
@@ -54,6 +54,7 @@ export class Compress extends Handler {
                 '-movflags +faststart',
                 '-codec:a aac',
                 '-b:a 256k',
+                '-filter:a volume=-1dB',
                 '-ar 48000',
                 '-threads 16',
                 this.outputFilePath()
