5d48d853c5df04733eab9f098539a8ba2519d532
Sergey Sadovoi <serg.sadovoi@gmail.com>
2017-01-12 10:29:46 +0200

    [fix] Close ssh connection after command execution

--- common/utils/ssh.js
+++ common/utils/ssh.js
@@ -1,96 +1,9 @@
 import App from 'server/server';
 import { Client } from 'ssh2';
-import { Transform } from 'stream';
 
 // TODO добавить выбор ноды для компрессии
 const services = App.get('services');
 const nodeConfig = services.compressorNodes.node;
-
-function isPrompt(line) {
-    return /:~# /.test(line);
-}
-
-function clearPrompt(line) {
-    return line.substring(line.indexOf(':~#') + 4);
-}
-// ea77f7f2ab45:~# [6n
-class NormalizedShellStream extends Transform {
-    constructor(command) {
-        super();
-
-        this.buffer = '';
-        this.commandBuffer = '';
-        this.isCommandSent = false;
-        this.isCommandOutput = false;
-        this.command = command.join(' ');
-    }
-
-    _transform(chunk, encoding, done) {
-        const self = this;
-        const lines = chunk.toString().split(/[\r\n]/);
-        const output = [];
-
-        if (lines.length > 1) {
-            lines.forEach(function(l, index) {
-                if (index === 0) {
-                    // First part, append it to the stub and push it
-                    output.push(self.buffer + l);
-                    self.buffer = '';
-                } else if (index === lines.length - 1) {
-                    // Last part of the chunk, this will be the new stub and the beginning
-                    // of the next chunk (until delimiter) will be appended to this
-                    self.buffer = l;
-                } else {
-                    // This must be a part cleanly separated by the delimiter within the
-                    // same chunk, push it
-                    output.push(l.trim());
-                }
-            });
-        } else {
-            // No delimiter found, append the chunk to the stub
-            if (isPrompt(lines[0])) {
-                output.push(lines[0]);
-            } else {
-                this.buffer = this.buffer + lines[0];
-            }
-        }
-
-        output.forEach((line) => {
-            if (isPrompt(line) && !this.isCommandSent) {
-                this.isCommandSent = true;
-                line = clearPrompt(line);
-            }
-
-            if (line.indexOf('[6n') !== -1) {
-                return;
-            }
-
-            if (isPrompt(line) && this.isCommandSent) {
-                self.end();
-                return;
-            }
-
-            if (this.isCommandSent && !this.isCommandOutput) {
-                this.commandBuffer += line;
-                if (this.command === this.commandBuffer) {
-                    this.isCommandOutput = true;
-                }
-                return;
-            }
-
-            if (this.isCommandOutput && line !== '') {
-                self.push(line);
-            }
-        });
-
-        done();
-    }
-
-    _flush(done) {
-        this.push(this.buffer);
-        done();
-    }
-}
 
 export default class SSH {
     constructor() {
@@ -116,20 +29,6 @@ export default class SSH {
         });
     }
 
-    async getShell() {
-        const conn = await this.getConnection();
-
-        return new Promise((resolve, reject) => {
-            conn.shell((err, stream) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                return resolve(stream);
-            });
-        });
-    }
-
     async exec (command, onData, onEnd) {
         const conn = await this.getConnection();
 
@@ -141,9 +40,18 @@ export default class SSH {
             }
 
             stream
-                .on('close', onEnd)
+                .on('close', () => {
+                    this.close();
+
+                    return onEnd();
+                })
                 .on('data', onData('stdout'))
                 .stderr.on('data', onData('stderr'));
         });
+    }
+
+    close() {
+        this.connection.end();
+        this.connection = null;
     }
 }
